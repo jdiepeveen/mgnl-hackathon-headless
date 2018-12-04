e466d67d71aa7e334b993f2b9bad50143df5d0af
Jordie Diepeveen <jordie.diepeveen@trimm.nl>
2018-12-04 20:06:59 +0100

    Added tasks for given anonymous access to website and dam workspaces

--- hackathon-headless-module/src/main/java/info/magnolia/hackathon2018/headless/setup/HeadlessModuleVersionHandler.java
+++ hackathon-headless-module/src/main/java/info/magnolia/hackathon2018/headless/setup/HeadlessModuleVersionHandler.java
@@ -1,6 +1,14 @@
 package info.magnolia.hackathon2018.headless.setup;
 
+import info.magnolia.cms.security.Permission;
 import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.AddPermissionTask;
+import info.magnolia.module.delta.Task;
+import info.magnolia.repository.RepositoryConstants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class is optional and lets you manage the versions of your module,
@@ -13,4 +21,17 @@ import info.magnolia.module.DefaultModuleVersionHandler;
  */
 public class HeadlessModuleVersionHandler extends DefaultModuleVersionHandler {
 
+    private final List<Task> updateTasks = new ArrayList<>();
+
+    @Override
+    protected List<Task> getExtraInstallTasks(InstallContext installContext) {
+        final List<Task> tasks = new ArrayList<>();
+
+        tasks.add(new AddPermissionTask("Allow 'anonymous' to access to 'website:/'", "rest-anonymous", RepositoryConstants.WEBSITE, "/", Permission.READ, true));
+        tasks.add(new AddPermissionTask("Allow 'anonymous' to access to 'dam:/'", "rest-anonymous", "dam", "/", Permission.READ, true));
+
+        tasks.addAll(updateTasks);
+
+        return tasks;
+    }
 }
